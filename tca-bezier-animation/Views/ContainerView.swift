813e0a9b38024fb0d8bbcd166d3df9cdabdb7158
MEI YIN LO <luomein@gmail.com>
2022-12-19 13:05:27 +0800

    2.0_1

---
+++ tca-bezier-animation/Views/ContainerView.swift
@@ -0,0 +1,29 @@
+//
+//  ContainerView.swift
+//  labPlayground
+//
+//  Created by MEI YIN LO on 2022/12/3.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+
+struct ContainerView: View{
+    let store: StoreOf<ContainerReducer>
+    var body: some View {
+        ContainerLayoutView(store: store, plottingViewBuilder: PlottingView.init, settingViewBuilder: SettingView.init)
+            
+    }
+}
+
+struct ContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+
+            ContainerView(store: Store(initialState: .init(), reducer: ContainerReducer()))
+                
+
+        
+    }
+}
+
