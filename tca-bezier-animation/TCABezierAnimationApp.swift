1911ad3d3fbd402c547daabda999738aca67222a
MEI YIN LO <luomein@gmail.com>
2022-12-19 13:07:59 +0800

    2.0_1

--- tca-bezier-animation/TCABezierAnimationApp.swift
+++ tca-bezier-animation/TCABezierAnimationApp.swift
@@ -10,16 +10,12 @@ import ComposableArchitecture
 
 @main
 struct TCABezierAnimationApp: App {
-//    @StateObject var environmentVariables : EnvironmentVariables = EnvironmentVariables()
 
     var body: some Scene {
         WindowGroup {
 
             ZStack{
-                //LabView(store: Store(initialState: .init(), reducer: LabReducer()))
                 ContainerView(store: Store(initialState: .init( ) , reducer: ContainerReducer()))
-                //PlottingView(store: Store(initialState: .init(controlPoints: MultipleTimeSeriesPointsReducer.State() ) , reducer: ContainerReducer()))
-                    //.environmentObject(environmentVariables)
             }
         }
     }
